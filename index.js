b4e7049639b50abab7b6a4e2c39e19963a98219a
Devesh <deveshwar00@gmail.com>
2021-08-24 22:07:53 +0530

    compares the typed input with para

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ xmlHttp.send(null);
 let para = xmlHttp.responseText;
 
 document.getElementById('paragraph').innerHTML = para;
+
+const inputElement = document.getElementById('input-area');
+
+function currentInput() {
+    let currentInput = inputElement.value;
+    let currentInputArr = currentInput.split('');
+    let charctersTyped = currentInputArr.length;
+    let paraArr = para.split('');
+    let charactersCorrect = 0;
+    paraArr.forEach((ch, i) => {
+        if(ch == currentInputArr[i])
+            charactersCorrect++;
+        else
+            break;
+    });
+    if(charctersTyped != charactersCorrect)
+        console.log("You typed wrong");
+    else
+        console.log("You typed correct");
+    console.log(currentInput);
+}
