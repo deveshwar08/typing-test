1aafdb32ffa8e0852e10e175909bea020f5bc6f0
Devesh <deveshwar00@gmail.com>
2021-08-25 07:34:06 +0530

    all basic features are implemented

--- index.js
+++ index.js
@@ -1,27 +1,81 @@
-var xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET",'http://metaphorpsum.com/paragraphs/2/4', false);
-xmlHttp.send(null);
-let para = xmlHttp.responseText;
+let s = 0;
+let m = 0;
+let h = 0;
+let testStarted = false;
+let testEnded = false;
+function resetValues(){
+    testEnded = false;
+    testStarted = false;
+    s = 0;
+    m = 0;
+    h = 0;
+    document.getElementById('result').innerText = '';
+    document.getElementById('paragraph').innerHTML = '';
+    getPara();
+    document.getElementById('input-area').value = '';
+}
+function timer(){
+    if(!testEnded){
+        s++;
+        if(s >= 60){
+            s -= 60;
+            m++;
+        }
+        if(m >= 60){
+            m -=60;
+            h++;
+        }
+        document.getElementById('timer').innerText = h+":"+m+":"+s;
+    }
+    setTimeout(timer,1000);
+}
 
-document.getElementById('paragraph').innerHTML = para;
+function getPara() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", 'http://metaphorpsum.com/paragraphs/1/10', false);
+    xmlHttp.send(null);
+    let para = xmlHttp.responseText;
+    para.split('').forEach(ch =>{
+        let span = document.createElement('span');
+        span.innerText = ch;
+        document.getElementById('paragraph').appendChild(span);
+    });
+}
 
 const inputElement = document.getElementById('input-area');
 
 function currentInput() {
     let currentInput = inputElement.value;
     let currentInputArr = currentInput.split('');
-    let charctersTyped = currentInputArr.length;
-    let paraArr = para.split('');
+    let paraArr = document.getElementsByTagName('span');
     let charactersCorrect = 0;
-    paraArr.forEach((ch, i) => {
-        if(ch == currentInputArr[i])
+    [...paraArr].forEach((ch, i) => {
+        if(currentInputArr[i] == null){
+            ch.classList.remove('red');
+            ch.classList.remove('green');
+        }else if (ch.innerText == currentInputArr[i]) {
             charactersCorrect++;
-        else
-            break;
+            ch.classList.remove('red');
+            ch.classList.add('green');
+        } else {
+            ch.classList.remove('green');
+            ch.classList.add('red');
+        }
     });
-    if(charctersTyped != charactersCorrect)
-        console.log("You typed wrong");
-    else
-        console.log("You typed correct");
-    console.log(currentInput);
+    if(charactersCorrect == [...paraArr].length){
+        testEnded = true;
+        let time = h*60*60 + m*60 + s;
+        let speed = [...paraArr].length / time;
+        gameOver(speed);
+    }
 }
+function startTest(){
+    if(!testStarted){
+        timer();
+        testStarted  =true;
+    }
+}
+function gameOver(speed){
+    document.getElementById('result').innerText = "Your speed: "+speed+" char/s";
+}
+getPara();
